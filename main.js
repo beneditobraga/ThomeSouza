89f412ef934fe6f8bc56cb62a991fd2bb82bdfac
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2024-04-23 09:37:31 -0300

    Update project

--- main.js
+++ main.js
@@ -8,7 +8,16 @@ function closeMenu(){
     ul.classList.remove('open');
 }
 
-const btndoe = document.getElementsByClassName('btndoe');
-const chavepix = document.getElementsByClassName('chavepix');
+const active = document.getElementById("active");
+const unactive = document.getElementById("unactive");
+const container = document.getElementById("container")
+
+active.addEventListener("click", () => {
+    container.classList.add("active");
+})
+
+unactive.addEventListener("click", () => {
+    container.remove("active");
+})
 
 

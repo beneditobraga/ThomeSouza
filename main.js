1a370d6e906ea89caaab86feca6fef0772dc9a37
Benedito Braga (Benê) <152809017+beneditobraga@users.noreply.github.com>
2024-04-24 19:12:21 -0300

    Update texts

--- main.js
+++ main.js
@@ -8,16 +8,3 @@ function closeMenu(){
     ul.classList.remove('open');
 }
 
-const active = document.getElementById("active");
-const unactive = document.getElementById("unactive");
-const container = document.getElementById("container")
-
-active.addEventListener("click", () => {
-    container.classList.add("active");
-})
-
-unactive.addEventListener("click", () => {
-    container.remove("active");
-})
-
-
